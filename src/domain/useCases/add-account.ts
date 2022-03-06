fe498ab0dcf831f1e0bacdd4dc25eaaac9868c86
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-03-06 16:47:34 -0300

    refactor: apply new eslint configs

--- src/domain/useCases/add-account.ts
+++ src/domain/useCases/add-account.ts
@@ -1,12 +1,12 @@
-import { AccountModel } from '../models/account'
+import { AccountModel } from "../models/account";
 
 // ? DTO?
 export interface AddAccountModel {
-  name: string
-  email: string
-  password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
 export interface AddAccount {
-  add: (account: AddAccountModel) => AccountModel
+  add: (account: AddAccountModel) => AccountModel;
 }
