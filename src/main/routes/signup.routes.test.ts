3a55f16cd91e54220229f12721c99f6a01c386d9
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-03-10 20:48:42 -0300

    test: ensure SignUp routes returns an account on success

---
+++ src/main/routes/signup.routes.test.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import request from 'supertest';
+
+import app from '../config/app';
+
+describe('SignUp Routes', () => {
+  test('Should return a new account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      })
+      .expect(200);
+  });
+});
