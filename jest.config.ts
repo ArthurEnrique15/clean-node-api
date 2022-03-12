b52cefbe31c1856e328959bf633ace22062a7808
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-03-12 16:26:45 -0300

    chore: remove coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
