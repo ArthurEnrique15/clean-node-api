b0b9420d31722ad3046a5f960ff4e3137ea7993a
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-09-02 14:13:17 -0300

    refactor: rename validator response variable and remove empty spaces

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -15,29 +15,21 @@ const makeSut = (): EmailValidatorAdapter => {
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = makeSut();
-
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
-
-    const isValid = sut.isValid('invalid_email@mail.com');
-
-    expect(isValid).toBe(false);
+    const response = sut.isValid('invalid_email@mail.com');
+    expect(response).toBe(false);
   });
 
   test('Should return true if validator returns true', () => {
     const sut = makeSut();
-
-    const isValid = sut.isValid('valid_email@mail.com');
-
-    expect(isValid).toBe(true);
+    const response = sut.isValid('valid_email@mail.com');
+    expect(response).toBe(true);
   });
 
   test('Should call validator with correct email', () => {
     const sut = makeSut();
-
     const isEmailSpy = jest.spyOn(validator, 'isEmail');
-
     sut.isValid('any_email@mail.com');
-
     expect(isEmailSpy).toBeCalledWith('any_email@mail.com');
   });
 });
